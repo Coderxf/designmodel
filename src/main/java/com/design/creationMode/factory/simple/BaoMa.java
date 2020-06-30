6ee9b88806d8300c607830764e2e7226a8b5105b
Coderxf <gl853211@gmail.com>
2020-06-30 23:57:52 +0800

    简单工厂

---
+++ src/main/java/com/design/creationMode/factory/simple/BaoMa.java
@@ -0,0 +1,13 @@
+package com.design.creationMode.factory.simple;
+
+/**
+ * 宝马基类
+ * Copyright (C), 2020-
+ *
+ * @author xufeng
+ * @create: 2020/6/30
+ */
+public class BaoMa {
+    public BaoMa() {
+    }
+}
